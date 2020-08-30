8f045eb744c6f9c4943ac0fdc1ef437a9992a63c
Baraza10699 <tbaraza23@amherst.edu>
2020-08-30 00:05:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -0,0 +1,26 @@
+package com.example.simpletodo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditActivity extends AppCompatActivity {
+
+
+    EditText etItem;
+    Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        etItem =findViewById(R.id.etitem);
+        btnSave = findViewById(R.id.btnSave);
+
+
+    }
+}
